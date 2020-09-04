273d532d39cc2608a2409605d3d33cbcb7648d49
StepanKobrii18321832 <esestepan2000@gmail.com>
2020-09-04 16:18:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = 'app-wrapper'>
+      <header className = 'app-header'>
       </header>
+      <nav className = 'app-navigation'>
+        <div>
+          <a> page 1 </a>
+        </div>
+        <div>
+          <a> page 2 </a>
+        </div>
+      </nav>
+      <div className = 'app-content'>
+        456
+      </div>
     </div>
   );
 }
